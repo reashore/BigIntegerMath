5c90521e79d2101c3c81fbdbeb238ad1afe924b1
Frank J. Reashore <reashore@telus.net>
2014-02-23 09:11:17 -0800

    added code

---
+++ BigIntegerMath.Test/BigIntegerMathLib.Test.cs
@@ -0,0 +1,33 @@
+﻿
+namespace BigIntegerMath.Test
+{
+    using NUnit.Framework;
+    using System.Numerics;
+    using BigIntegerMath;
+
+    [TestFixture]
+    public class BigIntegerMathLib
+    {
+        //private BigIntegerMathLib _bigIntegerMathLib;
+
+        //[SetUp]
+        //public void TestSetup()
+        //{
+        //    _bigIntegerMathLib = new BigIntegerMathLib();
+        //}
+
+        [Test]
+        public void TestBothVersionsOfFactorialAreEqual()
+        {
+            for (BigInteger n = 0; n < 100; n++)
+            {
+                // arrange
+                BigInteger factorial1 = BigIntegerMath.BigIntegerMathLib.Factorial1(n);
+                BigInteger factorial2 = BigIntegerMath.BigIntegerMathLib.Factorial2(n);
+
+                // assert
+                Assert.AreEqual(factorial1, factorial2);
+            }
+        }
+    }
+}
