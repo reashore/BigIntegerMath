90b45a774e3d58732b3963e4b0bc35b1fbc6acb5
Frank J. Reashore <reashore@telus.net>
2014-02-23 08:30:30 -0800

    domain changes

---
+++ BigIntegerMath/BigIntegerMathLib.cs
@@ -0,0 +1,35 @@
+﻿
+namespace BigIntegerMath
+{
+    using System.Numerics;
+    
+    public class BigIntegerMathLib
+    {
+        public static BigInteger Factorial1(BigInteger n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial1(n - 1);
+        }
+
+        public static BigInteger Factorial2(BigInteger n)
+        {
+            BigInteger factorial = 1;
+
+            if (n == 0)
+            {
+                return factorial;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+        }
+    }
+}
