e714bc75dfcb9110be0fbe1600a6d6ec4607a8ff
Frank J. Reashore <reashore@telus.net>
2016-05-21 13:12:39 -0700

    Added CRLFs

--- BigIntegerMath.Console/Program.cs
+++ BigIntegerMath.Console/Program.cs
@@ -12,14 +12,14 @@ namespace BigIntegerMath.Console
             BigInteger factorial1000 = BigIntegerMathLib.Factorial2(1000);
             BigInteger fibonacci1000 = BigIntegerMathLib.Fibonacci2(1000);
 
-            string message1 = string.Format("Factorial(1000) = {0}", factorial1000);
+            string message1 = string.Format("\nFactorial(1000) = \n{0}", factorial1000);
             Console.WriteLine(message1);
 
-            string message2 = string.Format("Fibonacci(1000) = {0}", fibonacci1000);
+            string message2 = string.Format("\nFibonacci(1000) = \n{0}", fibonacci1000);
             Console.WriteLine(message2);
 
             const string file = "FactorialAndFibonacci.txt";
-            string contents = string.Format("{0}\r\n\r\n{1}", message1, message2);
+            string contents = string.Format("{0}{1}", message1, message2);
             File.WriteAllText(file, contents);
 
             Console.ReadKey();
